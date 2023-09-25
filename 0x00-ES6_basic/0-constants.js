a635485c610b657976ad89f34d859d902dc6b6bd
printfCAT <cliffomanyo@gmail.com>
2023-09-25 16:24:35 +0300

    added documentation

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,16 @@
 export function taskFirst() {
+  // taskFirst function
   const task = 'I prefer const when I can.';
   return task;
 }
 
 export function getLast() {
+  // getLast function
   return ' is okay';
 }
 
 export function taskNext() {
+  // taskNext function
   let combination = 'But sometimes let';
   combination += getLast();
 
