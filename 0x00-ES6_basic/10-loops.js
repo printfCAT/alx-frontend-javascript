d68c29c5404251ce0d4e660ed42bf91114517343
printfCAT <cliffomanyo@gmail.com>
2023-09-25 18:41:47 +0300

    fixed task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
+  for (let idx in array) {
     let value = array[idx];
     array[idx] = appendString + value;
   }
