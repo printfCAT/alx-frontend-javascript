0dc609cd9c3ff652167001e38fb8dfef7713b2b9
printfCAT <cliffomanyo@gmail.com>
2023-09-27 00:53:58 +0300

    task 0 complete

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-unused-vars */
+export default function getResponseFromAPI() {
+  return Promise.resolve();
+}
