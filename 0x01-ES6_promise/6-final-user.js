2339de111c6256a70c682fec855abf769c7d6c3c
printfCAT <cliffomanyo@gmail.com>
2023-09-27 23:54:05 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((result) => ({
+      status: 'resolved',
+      value: result,
+    }))).catch((error) => [
+      {
+        status: 'rejected',
+        value: error,
+      },
+    ]);
+}
