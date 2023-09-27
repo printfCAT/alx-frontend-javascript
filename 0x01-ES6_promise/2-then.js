45e56ce07cb2b4dbee842f07cc84c5c3b8d85abb
printfCAT <cliffomanyo@gmail.com>
2023-09-27 17:57:21 +0300

    task 2 fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((res) => ({ status: 200, body: 'Success' }))
-    .catch((error) => new Error())
-    .finally((fin) => console.log('Got a response from the API'));
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
