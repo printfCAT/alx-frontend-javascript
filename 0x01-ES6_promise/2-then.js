ecd629082f4f389067aa2b3580e45d898add940a
printfCAT <cliffomanyo@gmail.com>
2023-09-27 17:37:06 +0300

    task 2 fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,5 +5,9 @@ export default function handleResponseFromAPI(promise) {
     const response = { status: 200, body: 'Success' };
     console.log('Got a response from the API');
     return response;
+  }).then((res) => {
+    const response2 = { status: 200, body: 'Success' };
+    console.log('Got a response from the API');
+    return response2;
   }).catch((error) => new Error());
 }
