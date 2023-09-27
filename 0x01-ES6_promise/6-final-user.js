67d4db13e684dda7af9078a70d5287f58fbd93ca
printfCAT <cliffomanyo@gmail.com>
2023-09-28 00:13:26 +0300

    task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => result.map((result) => ({
-      status: result.status,
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName)
+    .then((result) => ({
+      status: 'resolved',
       value: result,
-    }))).catch((error) => [
-      {
-        status: error.status,
-        value: error,
-      },
-    ]);
+    }));
+  const file = await uploadPhoto(fileName)
+    .catch((error) => ({
+      status: 'rejected',
+      value: error,
+    }));
+  return Promise.resolve([user, file]);
 }
