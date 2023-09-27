1e8d5e661c77869c8e5ef61db45fd31bd0362691
printfCAT <cliffomanyo@gmail.com>
2023-09-28 01:18:55 +0300

    Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw (new Error('cannot divide by 0'));
+  } else {
+    return numerator / denominator;
+  }
+}
