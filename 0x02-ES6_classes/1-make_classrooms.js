66f54c883dfa4b18f6fad558d4815cca2f1782f6
printfCAT <cliffomanyo@gmail.com>
2023-09-28 16:01:28 +0300

    task 1 done

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+}
