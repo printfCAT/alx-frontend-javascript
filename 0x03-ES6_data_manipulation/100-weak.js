f81dfdf3419d1c855541293a6f16fe17f8d5d5b3
printfCAT <cliffomanyo@gmail.com>
2023-10-03 22:48:24 +0300

    task 100 fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,12 @@
 /* eslint-disable linebreak-style */
-const weakMap = new WeakMap();
-export { weakMap };
+export const weakMap = new WeakMap();
+let count = 1;
 
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  const count = weakMap.get(endpoint);
-  weakMap.set(endpoint, count + 1);
-  if (count >= 5) {
+  weakMap.set(endpoint, count);
+  count += 1;
+  const queries = weakMap.get(endpoint);
+  if (queries >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
