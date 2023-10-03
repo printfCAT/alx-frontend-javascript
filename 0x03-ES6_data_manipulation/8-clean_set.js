c2e7ba4c2fd2b7c253fd9360e37b80152d597280
printfCAT <cliffomanyo@gmail.com>
2023-10-03 16:13:33 +0300

    task 8 fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,22 +1,15 @@
 /* eslint-disable linebreak-style */
 export default function cleanSet(set, startString) {
-  if (
-    typeof set !== 'object'
-      || typeof startString !== 'string'
-      || startString.length === 0) {
-    return '';
-  }
-  let result = '';
+  let text = '';
+  const array = [];
 
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result += `${value.slice(startString.length)}-`;
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
     }
+    text = array.join('-');
   }
-
-  if (result.endsWith('-')) {
-    result = result.slice(0, -1);
-  }
-
-  return result;
+  return text;
 }
