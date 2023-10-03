0d11ae69a52c7844a457e9c7adc3658cad84819a
printfCAT <cliffomanyo@gmail.com>
2023-10-03 22:32:53 +0300

    Weak link DS

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+const weakMap = new WeakMap();
+export { weakMap };
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const count = weakMap.get(endpoint);
+  weakMap.set(endpoint, count + 1);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
